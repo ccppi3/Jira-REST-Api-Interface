1463638b4473607280a2f05f56e921a530a98073
jonathan santis <jonathan.wyss@santismail.ch>
2025-01-24 16:31:09 +0100

    add some debug / logging options interface

--- pop3.py
+++ pop3.py
@@ -3,8 +3,30 @@ from getpass import getpass
 from email.parser import Parser
 from email.policy import default as default_policy
 from dotenv import load_dotenv
+from enum import Enum
 import os
+import json
 
+
+class err(Enum):
+    NONE = 0
+    ERROR = 1
+    DEBUG = 2
+    INFO = 3
+    def __int__(self):
+        return self.value
+DEBUG = err.INFO #1 error 2 debug 3 all
+
+def log(*kwargs,level=err.INFO):
+    if int(DEBUG) >= int(level):
+        string = ""
+        for arg in kwargs:
+            string = string + str(arg) + " "
+            print("DBG: ",string)
+
+def log_json(s):
+    if int(DEBUG) > 0:
+        print("----",json.dumps(s,indent=2,default=str),"\n")
 
 load_dotenv()
 
@@ -24,49 +46,54 @@ port = 995
 
 mailbox = poplib.POP3_SSL(host,port)
 
-print("Server Welcome:",mailbox.getwelcome())
+log("Server Welcome:",mailbox.getwelcome(),level=err.INFO)
 
 capa = mailbox.capa()
 
-print("Server capabilities: ", capa)
+log("Server capabilities: ", capa,level=err.INFO)
 
 resp = mailbox.user(user)
 if not password:
     password = getpass()
 else:
-    print("read password from .env")
+    log("read password from .env")
 
 resp = mailbox.pass_(password)
-print("response: ",resp)
+log("response: ",resp)
 
 maillist = mailbox.list()
 
-print("maillist: ",maillist)
+log("maillist: ",maillist)
 
 
-for mail in range(len(maillist[1])):
+for mail in range(len(maillist[1])): #maillist[0] contains the response, and maillist[1] contains the "octets" / bytestrea etc
     msg_str = ""
-    print("mail: ",mail)
-    msg = mailbox.retr(mail+1)
+    log("mail: ",mail)
+    msg = mailbox.retr(mail+1) #msg[0] is the response msg[1] is the data in the form of a line list
     for line in range(len(msg[1])):
-        msg_str = msg_str + msg[1][line].decode() + "\n"
+
+        try:
+            msg_str = msg_str + msg[1][line].decode() + "\n"
+        except:
+            log("Cant decode as a string, skipping and dumping the msg",err.ERROR)
+            log_json(msg)
 
     headers = Parser(policy=default_policy).parsestr(msg_str,headersonly=False)
 
     if mail_name in headers['from']:
-        print("From:",headers['from'])
-        print("Content type:",headers['Content-Type'])
-        print("Message: ",headers.get_payload(decode=True))
+        log("From:",headers['from'])
+        log("Content type:",headers['Content-Type'])
+        log("Message: ",headers.get_payload(decode=True))
         for part in headers.walk():
             ctype = part.get_content_type()
-            print(ctype)
+            log(ctype)
             if(ctype == "text/plain"):
-                print("content pt: ",part.get_content())
-        print("Message: ",headers.get_body())
+                log("content pt: ",part.get_content())
+        log("Message: ",headers.get_body())
 
         for att in headers.iter_attachments():
             atype = att.get_content_type()
-            print(atype)
+            log(atype,level=3)
             if "application/pdf" in atype:
                 pdffile = att.get_content() 
                 writefile(pdffile,outfile)
