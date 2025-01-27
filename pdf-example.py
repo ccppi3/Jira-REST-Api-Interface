700952fe6f968afcd2a7b0c837e41962873f032c
jonathan santis <jonathan.wyss@santismail.ch>
2025-01-27 15:02:17 +0100

    edit example to show how to process all tables on a page

--- pdf-example.py
+++ pdf-example.py
@@ -2,13 +2,14 @@ import pdf
 
 tables = pdf.Tables("tmp.pdf")
 tables.selectPage(0)
-tables.setTableNames(["Tabelle 1","NEUEINTRITT","Arbeitsplatzwechsel","NEUEINTRITTE"])
-tables.selectTable(0)
-tables.defRows(["b","Name","A"])
-tables.parseTable()
+listTable = tables.setTableNames(["Tabelle 1","NEUEINTRITT","Arbeitsplatzwechsel","NEUEINTRITTE"])
+for table in listTable:
+    tables.selectTableByObj(table)
+    tables.defRows(["b","Name","A"])
+    tables.parseTable()
 
 
-print("table 1")
-for i in tables.getObjectsFromTable():
-    print(i)
+    print("table 1")
+    for i in tables.getObjectsFromTable():
+        print(i)
 
