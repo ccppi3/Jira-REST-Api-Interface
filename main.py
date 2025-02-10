c5e60cfa9dd3fa438bbc0c4867affa275e374628
Jonathan Wyss <j.wyss@kolabnow.ch>
2025-02-10 10:42:09 +0100

    fix spelling error wrap code into funtion

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class TableData:
         self.pageNumber = pageNumber
         self.creationDate = creationDate
         
-PDFNAMEFILTER = os.gentenv('PdfNameFilter')
+PDFNAMEFILTER = os.getenv('PdfNameFilter')
 load_dotenv()
 
 pdf.setDebugLevel(err.ULTRA,_filter="")
@@ -45,113 +45,115 @@ port = os.getenv('Port')
 #uidsMail = pop3.getUidsMail(mailbox)
 #newAdded = pop3.addUidsDb(uidsMail)
 
-newAdded = []
-uidsMail = []
-log("parsing uids...")
-uids = com.getEntryIDDb()
-uidsMail = com.getEntryIDMail(filterName)
-if uidsMail:
-    newAdded = com.addEntryIDDb(uidsMail)
 
-if len(newAdded)==0:
-    log("No new mail, nothing to do",level=err.NONE)
 
-log("newadded")
-for uid in newAdded:
-    log("uid:",uid)
-    #iUid = uid.decode().split() #convert byte stream into two integers
-    log("type newadded:",type(uid))
-    log("newly added uid:",uid)
+def run():
+    newAdded = []
+    uidsMail = []
+    log("parsing uids...")
+    uids = com.getEntryIDDb()
+    uidsMail = com.getEntryIDMail(filterName)
+    if uidsMail:
+        newAdded = com.addEntryIDDb(uidsMail)
 
-#(response, ['mesgnum uid', ...], octets)
+    if len(newAdded)==0:
+        log("No new mail, nothing to do",level=err.NONE)
 
-newFileList = []
-for uid in newAdded:
-    #msgNum = uid.decode().split()[0]
-    log("new uid to parse:",uid)
-    #newFileList = newFileList + pop3.parseMail(mailbox,msgNum,filterName)
-    for att in com.getAttachements(uid):
-        if att.path not in newFileList:
-            newFileList.append(att)
-            log("Date:",att.creationDate)
-        else:
-            for i,att2 in enumerate(newFileList):
-                if att.path == att2.path:
-                    if att.creationDate > att2.creationDate:
-                        newFileList.pop(i)
-                        newFileList.append(att)
-                        break
+    log("newadded")
+    for uid in newAdded:
+        log("uid:",uid)
+        #iUid = uid.decode().split() #convert byte stream into two integers
+        log("type newadded:",type(uid))
+        log("newly added uid:",uid)
 
+
+    newFileList = []
+    for uid in newAdded:
+        #msgNum = uid.decode().split()[0]
+        log("new uid to parse:",uid)
+        #newFileList = newFileList + pop3.parseMail(mailbox,msgNum,filterName)
+        for att in com.getAttachements(uid):
+            if att.path not in newFileList:
+                newFileList.append(att)
+                log("Date:",att.creationDate)
+            else:
+                for i,att2 in enumerate(newFileList):
+                    if att.path == att2.path:
+                        if att.creationDate > att2.creationDate:
+                            newFileList.pop(i)
+                            newFileList.append(att)
+                            break
+    try:             
+        log("newFileList:",newFileList,level=err.ULTRA)
+    except:
+        log("no new Filelist",level=err.ULTRA)
+
+    log("done parsing, doing some filtering")
+    toBeRemoved = []
+    for i in range(0,len(newFileList)-1):
+        if i not in toBeRemoved:
+            for file2 in range(i+1,len(newFileList)):
+                log(newFileList[i].path," ",i," -> ",newFileList[file2].path," ",file2)
+                if newFileList[i].path == newFileList[file2].path:
+                    toBeRemoved.append(file2)
+                    log("Drop duble files: ",newFileList[i].path,"->",newFileList[file2].path,level=err.ERROR)
+
+    log("To be removed: ",toBeRemoved)
+    removeIndexesFromList(toBeRemoved,newFileList)
+    toBeRemoved.clear()
+
+    #filter out: is a plausible pdf?
+
+    #for i,fileName in enumerate(newFileList):
+    #    if PDFNAMEFILTER not in fileName:
+    #        toBeRemoved.append(i)
+    #log("To be removed: ",toBeRemoved)
+    #removeIndexesFromList(toBeRemoved,newFileList)
+    for a in newFileList:
+        log("Download :",a.path,a.uid)
+        com.downloadAttachements(a.uid)
+
+    log("\033[37;42m",level=err.NONE) #ansi escape sequence to change color
+    log("I checked ",len(list(uidsMail))," uids, processed ",len(newAdded)," new mails and downloaded ",len(newFileList)," files\033[0m",level=err.NONE);
+    log("\033[0m",level=err.NONE)#ansi reset color
+
+    tableDataList = []
+
+    for file in newFileList:
+        tables = pdf.Tables(file.path)
+        countPage = tables.countPages()
+        for pageNr in range(countPage):
+            log("Parse page ",pageNr," of file ",file,level=err.NONE)
+            tables.selectPage(pageNr)
+            listTable = tables.setTableNames(["Tabelle 1","NEUEINTRITT","Arbeitsplatzwechsel","NEUEINTRITTE"])
+            for i,table in enumerate(listTable):
+                tables.selectTableByObj(table)
+                tables.defRows(["Vorname","Name","Kürzel","Abteilung","Abteilung vorher","Abteilung neu","Abteilung Neu"])
+                tables.parseTable()
                 
-log("newFileList:",newFileList)
-log("done parsing, doing some filtering")
-toBeRemoved = []
-for i in range(0,len(newFileList)-1):
-    if i not in toBeRemoved:
-        for file2 in range(i+1,len(newFileList)):
-            log(newFileList[i].path," ",i," -> ",newFileList[file2].path," ",file2)
-            if newFileList[i].path == newFileList[file2].path:
-                toBeRemoved.append(file2)
-                log("Drop duble files: ",newFileList[i].path,"->",newFileList[file2].path,level=err.ERROR)
+                objList = []
+                for tbl in tables.getObjectsFromTable():
+                    objcpy = copy.deepcopy(tbl) # make a real copy of the data, else the data would be overwritten by the next page, as the data would be parsed as reference
+                    objList.append(objcpy)
+                tableDataList.append(TableData(table.name,objList,table.fileName,table.pageNumber,file.creationDate))
 
-log("To be removed: ",toBeRemoved)
-removeIndexesFromList(toBeRemoved,newFileList)
-toBeRemoved.clear()
+    for table in tableDataList:
+        filenameList = str(table.fileName).split("\\")
+        filename = filenameList[len(filenameList)-1]
+        print("--------¦",table.name,"¦-------------¦","filename: ",filename, "pageNr:", table.pageNumber,"Date: ",table.creationDate)
+        tempObjs = []
+        for entry in table.data:
+            print(entry)
+            tempObjs.append(entry)
+        print("-------------------------------------\n")
+        if "arbeitsplatzwechsel" in str(table.name).lower():
+            ticketTable = ticket.Ticket(tempObjs,filename,"Allpower","arbeitsplatzwechsel")
+        elif "neueintritte" in str(table.name).lower():
+            ticketTable = ticket.Ticket(tempObjs,filename,"Allpower","neueintritt")
+        elif "neueintritt" in str(table.name).lower():
+            ticketTable = ticket.Ticket(tempObjs,filename,"Allpower","neueintritte")
 
-#filter out: is a plausible pdf?
-
-#for i,fileName in enumerate(newFileList):
-#    if PDFNAMEFILTER not in fileName:
-#        toBeRemoved.append(i)
-#log("To be removed: ",toBeRemoved)
-#removeIndexesFromList(toBeRemoved,newFileList)
-for a in newFileList:
-    log("Download :",a.path,a.uid)
-    com.downloadAttachements(a.uid)
-
-log("\033[37;42m",level=err.NONE) #ansi escape sequence to change color
-log("I checked ",len(list(uidsMail))," uids, processed ",len(newAdded)," new mails and downloaded ",len(newFileList)," files\033[0m",level=err.NONE);
-log("\033[0m",level=err.NONE)#ansi reset color
-
-
-
-tableDataList = []
-
-for file in newFileList:
-    tables = pdf.Tables(file.path)
-    countPage = tables.countPages()
-    for pageNr in range(countPage):
-        log("Parse page ",pageNr," of file ",file,level=err.NONE)
-        tables.selectPage(pageNr)
-        listTable = tables.setTableNames(["Tabelle 1","NEUEINTRITT","Arbeitsplatzwechsel","NEUEINTRITTE"])
-        for i,table in enumerate(listTable):
-            tables.selectTableByObj(table)
-            tables.defRows(["Vorname","Name","Kürzel","Abteilung","Abteilung vorher","Abteilung neu","Abteilung Neu"])
-            tables.parseTable()
-            
-            objList = []
-            for tbl in tables.getObjectsFromTable():
-                objcpy = copy.deepcopy(tbl) # make a real copy of the data, else the data would be overwritten by the next page, as the data would be parsed as reference
-                objList.append(objcpy)
-            tableDataList.append(TableData(table.name,objList,table.fileName,table.pageNumber,file.creationDate))
-
-for table in tableDataList:
-    filenameList = str(table.fileName).split("\\")
-    filename = filenameList[len(filenameList)-1]
-    print("--------¦",table.name,"¦-------------¦","filename: ",filename, "pageNr:", table.pageNumber,"Date: ",table.creationDate)
-    tempObjs = []
-    for entry in table.data:
-        print(entry)
-        tempObjs.append(entry)
-    print("-------------------------------------\n")
-    if "arbeitsplatzwechsel" in str(table.name).lower():
-        ticketTable = ticket.Ticket(tempObjs,filename,"Allpower","arbeitsplatzwechsel")
-    elif "neueintritte" in str(table.name).lower():
-        ticketTable = ticket.Ticket(tempObjs,filename,"Allpower","neueintritt")
-    elif "neueintritt" in str(table.name).lower():
-        ticketTable = ticket.Ticket(tempObjs,filename,"Allpower","neueintritte")
-
-    ticketTable.create_ticket()
+        ticketTable.create_ticket()
     
-    
-
+if __name__=="__main__":
+    run()
