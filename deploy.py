8eeece4ff2d7bd2f07b6f2bf022caaf0ee07351e
Jonathan Wyss <j.wyss@kolabnow.ch>
2025-04-07 15:22:06 +0200

    Add functions for version checking against: git exe file

---
+++ deploy.py
@@ -0,0 +1,81 @@
+import sys,os
+import subprocess
+import win32api
+
+def amIBuilding():
+    try:
+        basePath = sys._MEIPASS # are we building?
+        return True
+    except:
+        return False
+
+def getVersionGit():
+    version = subprocess.run(["git","tag"],capture_output = True, text=True)
+    print("Git tells version (full):",version.stdout)
+    print("Git tells version (cut):",version.stdout)
+    return version.stdout
+
+def writeVersionFile(version, file="exeVersionInfo.txt"):
+    buffer = []
+    with open(file) as f:
+        stream = f.readlines()
+        for line in stream:
+            if "ProductVersion" in line:
+                print("line:",line)
+                posVersion = line.find("ProductVersion")
+                posVersion = posVersion + len("ProductVersion")
+
+                posNbr = line.find("u'",posVersion) + 2
+
+                line = list(line)
+                line = line[0:posNbr]
+                version = list(version)
+
+                for i,a in enumerate(version):
+                    line.append(a)
+                    #line[posNbr + i] = a    
+                sLine = slice(0,posNbr + i)
+                line = line[sLine]
+                line = ''.join(line)#create a string out of a list
+                line = line + "\')])\n"
+                print("line:",line)
+            buffer.append(line)
+    print("Buffer:",buffer)
+    
+    with open(file,"w") as f:
+        f.writelines(buffer)
+
+def getResourcePath(relPath):
+    try:
+        basePath = sys._MEIPASS
+    except Exception:
+        basePath = os.path.abspath(".")
+    return os.path.join(basePath,relPath)
+
+def getFileVersionExe(path=getResourcePath("jira-flow.exe")):
+    print("path to exe:",path)
+    try:
+        info = win32api.GetFileVersionInfo(path,'\\')
+    except Exception as e:
+        print("we dont seem to run as an exe, aborting version check\n",e)
+    else:
+        ls = info['ProductVersionLS']
+        ms = info['ProductVersionMS']
+        version = ".".join([str(win32api.HIWORD(ms)),str(win32api.LOWORD(ms)),str(win32api.HIWORD(ls))])
+        return version
+
+if amIBuilding():
+    version = getVersionGit()
+    writeVersionFile(version)
+
+if "__main__" in __name__:
+    version = getVersionGit()
+    print("version:",version)
+    writeVersionFile(version)
+    fileVersion = getFileVersionExe()
+    print(fileVersion)
+
+
+                
+
+
--- makefile
+++ makefile
@@ -1,12 +1,12 @@
 version = $(shell git describe --tags)
 build-pyinstaller:
 	@echo "build using using pyinstaller"
-	pyinstaller .\gui.py --windowed --noconfirm --name "jira-flow" --add-data "jira.ico;." --add-data ".env;." --add-data ".\TemplatePayload.json;." --add-data ".\help.txt;." --hiddenimport win32timezone --icon .\jira.ico
+	pyinstaller .\gui.py --version-file exeVersionInfo.txt --windowed --noconfirm --name "jira-flow" --add-data "jira.ico;." --add-data ".env;." --add-data ".\TemplatePayload.json;." --add-data ".\help.txt;." --hiddenimport win32timezone --icon .\jira.ico
 	@echo "to make an installer run make installer, this will generate a setup.exe in the directory Output"
 	@echo "to make a zip run make zip"
 build-pyinstaller-debug:
 	@echo "build using using pyinstaller"
-	pyinstaller .\gui.py --noconfirm --name "jira-flow" --add-data "jira.ico;." --add-data ".env;." --add-data ".\TemplatePayload.json;." --add-data ".\help.txt;." --hiddenimport win32timezone --icon .\jira.ico
+	pyinstaller .\gui.py --version-file exeVersionInfo.txt --noconfirm --name "jira-flow" --add-data "jira.ico;." --add-data ".env;." --add-data ".\TemplatePayload.json;." --add-data ".\help.txt;." --hiddenimport win32timezone --icon .\jira.ico
 	@echo "to make an installer run make installer, this will generate a setup.exe in the directory Output"
 	@echo "to make a zip run make zip"
 zip:
